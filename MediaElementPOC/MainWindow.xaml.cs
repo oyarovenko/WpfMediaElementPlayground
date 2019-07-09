35f324466e168feceac17864a15d710f1d027c1c
oyarovenko <oyarovenko@lohika.com>
2019-07-09 16:52:03 +0300

    first commit

---
+++ MediaElementPOC/MainWindow.xaml.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MediaElementPOC
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private enum VideoState
+        {
+            Undefined,
+            Playing,
+            Paused,
+            Stopped
+        }
+
+        private const double ButtonsOpacity = 0.5;
+        private const string TimestampFormat = @"hh\:mm\:ss";
+
+        private Timer _timer;
+        private VideoState _mediaElementState = VideoState.Undefined;
+
+        private int _seekInterval = 10;
+        public string SeekIntervalText
+        {
+            get => _seekInterval.ToString();
+            set
+            {
+                if (int.TryParse(value, out int interval))
+                {
+                    _seekInterval = interval;
+                }
+            }
+        } 
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+            SetPlaybackButtonsState(true, false, false);
+
+            _timer = new Timer(TimestampLabelUpdateTimerTicked);
+
+            mediaPanel.MediaOpened += MediaPanelMediaOpened;
+
+            KeyDown += OnMediaElementKeyDown;
+            Closing += MainWindowClosing;
+        }
+
+        private void MediaPanelMediaOpened(object sender, RoutedEventArgs e)
+        {
+            if(mediaPanel.NaturalDuration.HasTimeSpan)
+            {
+                totalTimestampLabel.Content = mediaPanel.NaturalDuration.TimeSpan.ToString(TimestampFormat);
+            }
+
+            sizeLabel.Content = $"Size: {mediaPanel.NaturalVideoWidth}x{mediaPanel.NaturalVideoHeight}";
+        }
+
+        private void TimestampLabelUpdateTimerTicked(object state)
+        {
+            Dispatcher.Invoke(() => currentTimestampLabel.Content = mediaPanel.Position.ToString(TimestampFormat));
+        }
+
+        private void MainWindowClosing(object sender, CancelEventArgs e)
+        {
+            KeyDown -= OnMediaElementKeyDown;
+
+            _timer?.Change(Timeout.Infinite, Timeout.Infinite);
+            _timer?.Dispose();
+        }
+
+        private void ButtonHelpClick(object sender, RoutedEventArgs e)
+        {
+            var helpWindow = new HelpWindow();
+            helpWindow.ShowDialog();
+        }
+
+        private void OnMediaElementKeyDown(object sender, KeyEventArgs e)
+        {
+            if (!(_mediaElementState == VideoState.Playing || _mediaElementState == VideoState.Paused))
+                return;
+
+            switch (e.Key)
+            {
+                case Key.Down: mediaPanel.Volume -= 0.1;
+                    break;
+
+                case Key.Up: mediaPanel.Volume += 0.1;
+                    break;
+
+                case Key.Right: mediaPanel.Position += TimeSpan.FromSeconds(_seekInterval);
+                    break;
+
+                case Key.Left: mediaPanel.Position -= TimeSpan.FromSeconds(_seekInterval);
+                    break;
+
+                case Key.M:
+                    {
+                        mediaPanel.IsMuted = !mediaPanel.IsMuted;
+                        muteLabel.Visibility = mediaPanel.IsMuted ? Visibility.Visible : Visibility.Hidden;
+                    }
+                    break;
+            }
+        }
+
+        private void PlayVideo()
+        {
+            if (_mediaElementState != VideoState.Playing)
+            {
+                mediaPanel.Play();
+                _mediaElementState = VideoState.Playing;
+
+                SetPlaybackButtonsState(false, true, true);
+
+                _timer.Change(0, 500);
+            }
+        }
+
+        private void SetPlaybackButtonsState(bool playEnabled, bool pauseEnabled, bool stopEnabled)
+        {
+            playButton.IsEnabled = playEnabled;
+            pauseButton.IsEnabled = pauseEnabled;
+            stopButton.IsEnabled = stopEnabled;
+
+            seekIntervalValue.IsEnabled = playEnabled;
+            uri.IsEnabled = playEnabled;
+        }
+
+        #region Playback Buttons Click Handlers
+        private void ButtonPlayClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(uri.Text))
+                {
+                    if (_mediaElementState != VideoState.Paused)
+                    {
+                        mediaPanel.LoadedBehavior = MediaState.Manual;
+                        mediaPanel.Source = new Uri(uri.Text);
+                    }
+                    
+                    PlayVideo();
+                }
+                else
+                {
+                    MessageBox.Show("Video URI should not be empty", "Invalid URI", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Unable to load the video: {ex.Message}", "Loading Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void ButtonPauseClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (_mediaElementState != VideoState.Paused)
+                {
+                    mediaPanel.Pause();
+                    _mediaElementState = VideoState.Paused;
+
+                    SetPlaybackButtonsState(true, false, true);
+
+                    _timer.Change(Timeout.Infinite, Timeout.Infinite);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Unable to pause the video: {ex.Message}", "Pausing Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }  
+        }
+
+        private void ButtonStopClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (_mediaElementState != VideoState.Stopped)
+                {
+                    mediaPanel.Stop();
+                    _mediaElementState = VideoState.Stopped;
+
+                    SetPlaybackButtonsState(true, true, false);
+
+                    _timer.Change(Timeout.Infinite, Timeout.Infinite);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Unable to stop the video: {ex.Message}", "Stopping Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        #endregion
+
+        #region Mouse Enter/Leave Click Handlers
+        private void ButtonStartMouseEnter(object sender, MouseEventArgs e)
+        {
+            playButton.Opacity = 1;
+        }
+
+        private void ButtonStartMouseLeave(object sender, MouseEventArgs e)
+        {
+            playButton.Opacity = ButtonsOpacity;
+        }
+
+        private void ButtonPauseMouseEnter(object sender, MouseEventArgs e)
+        {
+            pauseButton.Opacity = 1;
+        }
+
+        private void ButtonPauseMouseLeave(object sender, MouseEventArgs e)
+        {
+            pauseButton.Opacity = ButtonsOpacity;
+        }
+
+        private void ButtonStopMouseEnter(object sender, MouseEventArgs e)
+        {
+            stopButton.Opacity = 1;
+        }
+
+        private void ButtonStopMouseLeave(object sender, MouseEventArgs e)
+        {
+            stopButton.Opacity = ButtonsOpacity;
+        }
+
+        #endregion
+
+        private void SeekIntervalValueKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter || e.Key == Key.Escape)
+            {
+                seekIntervalValue.MoveFocus(new TraversalRequest(FocusNavigationDirection.Down));
+            }
+        }
+    }
+}
---
+++ ProxyHttpsRedirectTest/MainWindow.xaml.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //private const string RemoteUrl = @"https://video-dev.github.io/streams/x36xhzz/url_8/url_596/";
+        //private const string ResourceName = @"193039199_mp4_h264_aac_fhd_7.ts";
+
+        private const string RemoteUrl = @"https://commondatastorage.googleapis.com/gtv-videos-bucket/sample";
+        private const string ResourceName = "ForBiggerBlazes.mp4"; // 00:15
+        //private const string ResourceName = "TearsOfSteel.mp4"; //super long - 12:44
+        private const string ServerPath = @"http://localhost:8573/";
+
+        private HttpListener _server;
+        private HttpClient _client;
+        private CancellationTokenSource _cts;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _server = new HttpListener();
+            _server.Prefixes.Add(ServerPath);
+            _server.Start();
+
+            _client = new HttpClient();
+            _cts = new CancellationTokenSource();
+
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+
+            Closing += OnWinClosing;
+        }
+
+        private void ButtonLoadClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                mediaPanel.Source = new Uri($"{ServerPath}/{ResourceName}");
+                _server.BeginGetContext(OnMessageArrived, _server);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        private void OnWinClosing(object sender, CancelEventArgs e)
+        {
+            _cts?.Cancel();
+            _client?.Dispose();
+            _server?.Stop();
+        }
+
+        #region HTTP Client
+
+        private async Task<HttpResponseMessage> HeadVideoRequest(string resourceName)
+        {
+            var result = await _client.SendAsync(
+                new HttpRequestMessage
+                {
+                    Method = HttpMethod.Head,
+                    RequestUri = new Uri($"{RemoteUrl}{resourceName}"),
+                    Version = new Version(1, 1),
+                }, _cts.Token);
+
+            return result;
+        }
+
+        private async Task<HttpResponseMessage> GetVideoRequest(string resourceName)
+        {
+            var result = await _client.SendAsync(
+                new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri($"{RemoteUrl}{resourceName}"),
+                    Version = new Version(1, 1),
+                }, HttpCompletionOption.ResponseHeadersRead, _cts.Token);
+
+            return result;
+        }
+
+        #endregion
+
+        #region HTTP Server
+
+        private async void OnMessageArrived(IAsyncResult ar)
+        {
+            var listiner = (HttpListener)ar.AsyncState;
+            HttpListenerContext context = listiner.EndGetContext(ar);
+
+            var request = context.Request;
+            string resourceName = request.RawUrl;
+
+            var getVideoResponce = await GetVideoRequest(resourceName);
+            getVideoResponce.EnsureSuccessStatusCode();
+
+            using (var output = context.Response.OutputStream)
+            {
+                using (var networkStream = await getVideoResponce.Content.ReadAsStreamAsync())
+                {
+                    _cts.Token.Register(() =>
+                    {
+                        output.Close();
+                        networkStream.Close();
+                    });
+
+                    //cancellation still not working as expected, consider other options for copying or handle cancelleation exceptions
+                    await networkStream.CopyToAsync(output, 4096, _cts.Token);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
